a06a7a5a6bb9388e926b1467e9f761a36cb574ff
avloko <gabriel.felicissimo@outlook.com>
2025-07-21 15:47:07 -0300

    add controllers/models

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// Iniciar sessão para carrinho e cupons
+session_start();
+
+// Carregar roteador
+require_once __DIR__ . '/../app/core/Router.php';
+
+$router = new Router();
+
+// Registrar rotas GET
+$router->get('/produtos', 'ProdutoController@index');
+$router->get('/produtos/novo', 'ProdutoController@create');
+$router->get('/produtos/editar', 'ProdutoController@edit');
+$router->get('/carrinho', 'CarrinhoController@index');
+$router->get('/pedido-finalizado', 'PedidoController@finalizado');
+
+// Registrar rotas POST
+$router->post('/produtos/salvar', 'ProdutoController@store');
+$router->post('/produtos/atualizar', 'ProdutoController@update');
+$router->post('/carrinho/adicionar', 'CarrinhoController@adicionar');
+$router->post('/carrinho/remover', 'CarrinhoController@remover');
+$router->post('/carrinho/aplicar-cupom', 'CarrinhoController@aplicarCupom');
+$router->post('/pedido/finalizar', 'PedidoController@checkout');
+
+// Disparar rota atual
+$router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../app/core/Router.php';
-require_once '../app/core/Database.php';
-require_once '../app/core/Controller.php';
-require_once '../app/core/Helpers.php';
-require_once '../app/core/Session.php';
-
-$router = new Router();
-$router->run();
