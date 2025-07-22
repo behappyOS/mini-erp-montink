e3df737d6d116fcc6dabdc4757633e02c16bbdf0
avloko <gabriel.felicissimo@outlook.com>
2025-07-22 01:33:24 -0300

    add routes

--- index.php
+++ index.php
@@ -11,22 +11,6 @@ if ($_SERVER['REQUEST_URI'] === '/' || $_SERVER['REQUEST_URI'] === '') {
     exit;
 }
 
-$router->get('/produtos', 'ProdutoController@index');
-$router->get('/produtos/novo', 'ProdutoController@create');
-$router->get('/produtos/editar', 'ProdutoController@edit');
-$router->get('/carrinho', 'CarrinhoController@index');
-$router->get('/pedido-finalizado', 'PedidoController@finalizado');
-$router->get('/coupons', 'CupomController@index');
-
-$router->post('/coupons/store', 'CupomController@store');
-$router->post('/coupons/delete', 'CupomController@delete');
-$router->post('/coupons/validar', 'CupomController@validar');
-$router->post('/produtos/salvar', 'ProdutoController@store');
-$router->post('/produtos/atualizar', 'ProdutoController@update');
-$router->post('/carrinho/adicionar', 'CarrinhoController@adicionar');
-$router->post('/carrinho/remover', 'CarrinhoController@remover');
-$router->post('/carrinho/aplicar-cupom', 'CarrinhoController@aplicarCupom');
-$router->post('/pedido/finalizar', 'PedidoController@checkout');
-$router->post('/pedido/webhook', 'PedidoController@webhook');
+require_once __DIR__ . '/routes/web.php';
 
 $router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
